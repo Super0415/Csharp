e1ed0946474edd8b097c074847b337950b4849fe
zc <zc>
2019-07-11 14:15:31 +0800

    记录：MyTest00网口检测完成

--- MyTest00/MyTest00/Form1.Designer.cs
+++ MyTest00/MyTest00/Form1.Designer.cs
@@ -28,26 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.连接方式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定1ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定2ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.未定3ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.中英切换ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.技术支持ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.版本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(192, 16);
+            this.button1.Location = new System.Drawing.Point(303, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "未连接";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 18);
+            this.textBox1.Location = new System.Drawing.Point(197, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 1;
@@ -55,50 +78,230 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 21);
+            this.label1.Location = new System.Drawing.Point(168, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "IP ";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(322, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "权限密码";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(381, 16);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 4;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(505, 14);
+            this.button2.Location = new System.Drawing.Point(623, 1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "登录";
+            this.button2.Text = "未连接";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 388);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(699, 231);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "端口";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.连接方式ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(728, 25);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // 连接方式ToolStripMenuItem
+            // 
+            this.连接方式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.未定1ToolStripMenuItem,
+            this.未定2ToolStripMenuItem,
+            this.未定3ToolStripMenuItem,
+            this.未定4ToolStripMenuItem});
+            this.连接方式ToolStripMenuItem.Name = "连接方式ToolStripMenuItem";
+            this.连接方式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.连接方式ToolStripMenuItem.Text = "菜单";
+            // 
+            // 未定1ToolStripMenuItem
+            // 
+            this.未定1ToolStripMenuItem.Name = "未定1ToolStripMenuItem";
+            this.未定1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.未定1ToolStripMenuItem.Text = "未定1";
+            // 
+            // 未定2ToolStripMenuItem
+            // 
+            this.未定2ToolStripMenuItem.Name = "未定2ToolStripMenuItem";
+            this.未定2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.未定2ToolStripMenuItem.Text = "未定2";
+            // 
+            // 未定3ToolStripMenuItem
+            // 
+            this.未定3ToolStripMenuItem.Name = "未定3ToolStripMenuItem";
+            this.未定3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.未定3ToolStripMenuItem.Text = "未定3";
+            // 
+            // 未定4ToolStripMenuItem
+            // 
+            this.未定4ToolStripMenuItem.Name = "未定4ToolStripMenuItem";
+            this.未定4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.未定4ToolStripMenuItem.Text = "未定4";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.未定1ToolStripMenuItem1,
+            this.未定2ToolStripMenuItem1,
+            this.未定3ToolStripMenuItem1});
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 未定1ToolStripMenuItem1
+            // 
+            this.未定1ToolStripMenuItem1.Name = "未定1ToolStripMenuItem1";
+            this.未定1ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.未定1ToolStripMenuItem1.Text = "未定1";
+            // 
+            // 未定2ToolStripMenuItem1
+            // 
+            this.未定2ToolStripMenuItem1.Name = "未定2ToolStripMenuItem1";
+            this.未定2ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.未定2ToolStripMenuItem1.Text = "未定2";
+            // 
+            // 未定3ToolStripMenuItem1
+            // 
+            this.未定3ToolStripMenuItem1.Name = "未定3ToolStripMenuItem1";
+            this.未定3ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.未定3ToolStripMenuItem1.Text = "未定3";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.中英切换ToolStripMenuItem,
+            this.技术支持ToolStripMenuItem,
+            this.版本ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 中英切换ToolStripMenuItem
+            // 
+            this.中英切换ToolStripMenuItem.Name = "中英切换ToolStripMenuItem";
+            this.中英切换ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.中英切换ToolStripMenuItem.Text = "中英切换";
+            // 
+            // 技术支持ToolStripMenuItem
+            // 
+            this.技术支持ToolStripMenuItem.Name = "技术支持ToolStripMenuItem";
+            this.技术支持ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.技术支持ToolStripMenuItem.Text = "技术支持";
+            // 
+            // 版本ToolStripMenuItem
+            // 
+            this.版本ToolStripMenuItem.Name = "版本ToolStripMenuItem";
+            this.版本ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.版本ToolStripMenuItem.Text = "版本";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "端口1",
+            "端口2",
+            "端口3",
+            "端口4",
+            "端口5",
+            "端口6",
+            "端口7",
+            "端口8"});
+            this.comboBox1.Location = new System.Drawing.Point(445, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 20);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(510, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "波特率";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(557, 1);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(60, 20);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(51, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "开启计时";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(51, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "测试发送";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1250, 631);
+            this.ClientSize = new System.Drawing.Size(728, 512);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "S2Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +312,29 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 连接方式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 未定1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 未定2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 未定3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 未定4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 未定1ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 未定2ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 未定3ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 中英切换ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 技术支持ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 版本ToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- MyTest00/MyTest00/Form1.cs
+++ MyTest00/MyTest00/Form1.cs
@@ -7,14 +7,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
 
 namespace MyTest00
 {
     public partial class Form1 : Form
     {
+        private TcpClient client;                   //创建一个客户端
+        NetworkStream sendStream;                   //创建一个数据流
+        private const int buffSize = 10240;         //设置数据流大小为1024
+        private int Connectstate = 0;               //记录端口连接状态
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox3.Height = 100;
+            this.textBox3.Width = 700;
+            this.textBox1.Text = "192.168.1.100";
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        //点击事件 - 网口连接
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress serverIp = IPAddress.Parse(this.textBox1.Text);  //服务端IP 将IP字符串转化为IPAddress实例  注：未填写IP地址，此处阻塞
+            TcpClient client = new TcpClient(); //实例化客户端
+            
+            if (Connectstate == 0)
+            {
+                client.Connect(serverIp, 4000); //端口8888需与服务端开启的端口一致，否则无法与服务端建立链接    注：未链接服务器此处阻塞
+                this.textBox3.AppendText(DateTime.Now.ToString() + " ");
+                this.textBox3.AppendText("网口打开链接" + "\r\n");
+                this.button1.Text = "已连接";
+                Connectstate = 1;
+            }
+            else
+            {
+                client.Close();
+                this.textBox3.AppendText(DateTime.Now.ToString() + " ");
+                this.textBox3.AppendText("网口断开链接" + "\r\n");
+                this.button1.Text = "未连接";
+                Connectstate = 0;
+            }
+        }
+
+        //获取所有网络端口信息
+        public void GetTcpConnections()
+        {
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();  
+            foreach (TcpConnectionInformation t in connections)
+            {
+                this.textBox3.AppendText("Local endpoint: " + t.LocalEndPoint.ToString() + "\r\n");
+                this.textBox3.AppendText("Remote endpoint:" + t.RemoteEndPoint.ToString() + "\r\n");
+            }
+        }
+
+        //获取指定IP端口物理状态
+        public void GetTcpPOPnnections()
+        {
+            Ping p = new Ping();
+            PingReply reply = p.Send(this.textBox1.Text);   //进行 ping 连接测试，并返回连接状态
+            if (reply.Status == IPStatus.Success)
+            {
+                this.textBox3.AppendText(DateTime.Now.ToString() + " ");
+                this.textBox3.AppendText("物理链路连接成功" + "\r\n");
+                Connectstate = 1;
+            }
+            else
+            {
+                this.textBox3.AppendText(DateTime.Now.ToString() + " ");
+                this.textBox3.AppendText("物理链路连接失败" + "\r\n");
+                this.button1.Text = "未连接";
+                Connectstate = 0;
+            };
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //this.textBox3.AppendText(DateTime.Now.ToString() + " ");
+            //GetTcpConnections();
+            GetTcpPOPnnections();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) timer1.Enabled = false;
+            else timer1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        //点击事件 - 串口连接（暂未写）
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
