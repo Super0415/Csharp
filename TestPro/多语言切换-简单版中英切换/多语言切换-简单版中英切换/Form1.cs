f6535200bef5d05766496faa48d562c90f70a06a
zc <zc>
2019-09-27 15:28:08 +0800

    记录：演示代码上传

---
+++ TestPro/多语言切换-简单版中英切换/多语言切换-简单版中英切换/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 多语言切换_简单版中英切换
+{
+    public partial class Form1 : Form
+    {
+        protected int language = 0;
+        languages lang = new languages();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            language++;
+            if (language > 2) language = 0;
+
+            this.Text = lang.FormTest[language];
+            label1.Text = lang.Lb1Test[language];
+            button1.Text = lang.Btn1Test[language];
+        }
+    }
+
+    /// <summary>
+    /// 新建语言类
+    /// </summary>
+    public class languages
+    {
+        public string[] FormTest = { "中文窗体", "English Form" , "한글" };
+        public string[] Lb1Test = { "中文", "English", "한글" };
+        public string[] Btn1Test = { "语言", "Language", "언어" };
+    }
+}
---
+++ TestPro/多语言切换-简单版中英切换/多语言切换-简单版中英切换/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 多语言切换_简单版中英切换
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
