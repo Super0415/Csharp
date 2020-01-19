51999e916fcfc97c841d2dda878c98887a5af08f
zc <zc>
2020-01-19 18:37:02 +0800

    记录：调试跨线程调用控件的方法

---
+++ TestPro/跨线程调用控件/跨线程调用控件/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace 跨线程调用控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.开启线程 = new System.Windows.Forms.Button();
+            this.关闭线程 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 400);
+            this.textBox1.TabIndex = 0;
+            // 
+            // 开启线程
+            // 
+            this.开启线程.Location = new System.Drawing.Point(62, 21);
+            this.开启线程.Name = "开启线程";
+            this.开启线程.Size = new System.Drawing.Size(75, 23);
+            this.开启线程.TabIndex = 1;
+            this.开启线程.Text = "开启线程";
+            this.开启线程.UseVisualStyleBackColor = true;
+            this.开启线程.Click += new System.EventHandler(this.开启线程_Click);
+            // 
+            // 关闭线程
+            // 
+            this.关闭线程.Location = new System.Drawing.Point(206, 21);
+            this.关闭线程.Name = "关闭线程";
+            this.关闭线程.Size = new System.Drawing.Size(75, 23);
+            this.关闭线程.TabIndex = 2;
+            this.关闭线程.Text = "关闭线程";
+            this.关闭线程.UseVisualStyleBackColor = true;
+            this.关闭线程.Click += new System.EventHandler(this.关闭线程_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 462);
+            this.Controls.Add(this.关闭线程);
+            this.Controls.Add(this.开启线程);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button 开启线程;
+        private System.Windows.Forms.Button 关闭线程;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TestPro/跨线程调用控件/跨线程调用控件/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace 跨线程调用控件
+{
+    //代码示例1
+    /*
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 任务线程
+        /// </summary>
+        List<Thread> Thread_List = new List<Thread>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            #region method-1
+            Control.CheckForIllegalCrossThreadCalls = false;
+            #endregion
+        }
+
+        /// <summary>
+        /// 记录信息
+        /// </summary>
+        /// <param name="info"></param>
+        private void RecordInfo(string info)
+        {
+            textBox1.AppendText(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff \r\n") + info +"\r\n");
+        }
+             
+        private void 开启线程_Click(object sender, EventArgs e)
+        {
+            RecordInfo("开启线程");
+            Thread thread1 = new Thread(Task_Thread);
+            thread1.Start();
+            if (Thread_List.IndexOf(thread1) == -1)
+            {
+                Thread_List.Add(thread1);
+            }
+        }
+
+        /// <summary>
+        /// 任务：打印当前线程ID
+        /// </summary>
+        private void Task_Thread()
+        {
+            while (Thread.CurrentThread.ThreadState != ThreadState.Aborted)
+            {
+                RecordInfo("当前线程ID：" + Thread.CurrentThread.ManagedThreadId.ToString()+ "状态："+ Thread.CurrentThread.IsAlive.ToString());
+                Thread.Sleep(1000);
+            }        
+        }      
+        private void 关闭线程_Click(object sender, EventArgs e)
+        {
+            foreach (Thread item in Thread_List)
+            {
+                RecordInfo("当前线程ID：" + item.ManagedThreadId.ToString() + " 关闭！");
+                item.Abort();
+                Thread_List.Remove(item);
+            }
+        }
+
+        /// <summary>
+        /// 强制关闭多线程
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+    */
+
+
+    //代码示例2
+    ///*
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 任务线程
+        /// </summary>
+        List<Thread> Thread_List = new List<Thread>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 记录信息
+        /// </summary>
+        /// <param name="info"></param>
+        private void RecordInfo(object info)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                // 当一个控件的InvokeRequired属性值为真时，说明有一个创建它以外的线程想访问它
+                Action<string> actionDelegate = (x) => { this.textBox1.AppendText(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff \r\n") + x.ToString() + "\r\n"); };
+
+                this.textBox1.Invoke(actionDelegate, info);
+            }
+            else
+            {
+                this.textBox1.AppendText(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff \r\n") + info.ToString() + "\r\n");
+            }
+        }
+        private void 开启线程_Click(object sender, EventArgs e)
+        {
+            RecordInfo("开启线程");
+            Thread thread1 = new Thread(Task_Thread);
+            thread1.Start();
+            if (Thread_List.IndexOf(thread1) == -1)
+            {
+                Thread_List.Add(thread1);
+            }
+        }
+
+        /// <summary>
+        /// 任务：打印当前线程ID
+        /// </summary>
+        private void Task_Thread()
+        {
+            while (Thread.CurrentThread.ThreadState != ThreadState.Aborted)
+            {
+                RecordInfo("当前线程ID：" + Thread.CurrentThread.ManagedThreadId.ToString() + "状态：" + Thread.CurrentThread.IsAlive.ToString());
+
+                Thread.Sleep(1000);
+            }
+        }
+        private void 关闭线程_Click(object sender, EventArgs e)
+        {
+            for (int i = 0;i<Thread_List.Count;i++)
+            {
+                Thread item = Thread_List[i];
+                RecordInfo("当前线程ID：" + item.ManagedThreadId.ToString() + " 关闭！");
+                item.Abort();
+            }
+            Thread_List.ForEach(c=> c.Abort());
+        }
+
+        /// <summary>
+        /// 强制关闭多线程
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+
+    //*/
+
+
+
+
+
+
+
+
+    #region method-1
+    /*
+    public Form1()
+    {
+        InitializeComponent();
+        #region method-1
+        Control.CheckForIllegalCrossThreadCalls = false;
+        #endregion
+    }
+    */
+    #endregion
+
+}
