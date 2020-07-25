77651b75faf27b271399047f9a360a692a3a78de
zc <zc>
2020-07-25 15:18:54 +0800

    记录：同步C#记录，添加USB例程

---
+++ TestPro/获取可选文件路径/获取可选文件路径/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 获取可选文件路径
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fName = openFileDialog1.FileName;
+                this.Text = fName;
+            }
+        }
+    }
+}
